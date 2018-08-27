fe003145ba7ef739a56511a19457093fdfeae0c4
mizt <info@mizt.org>
2018-08-27 14:25:33 +0900

    0.0.2

--- interimreport-cli.rb
+++ interimreport-cli.rb
@@ -1,8 +1,8 @@
 class InterimreportCli < Formula
-	url "https://github.com/tokisatomiztsuru/interimreport-cli/archive/0.0.1.tar.gz"
-	sha256 "3acd5510a22004803603cd32b06bcb7d3e4ff4a2bfa8a02e7190c3f7f5dbc4df"
-    version "0.0.1"
-	def install
-		bin.install "./bin/interimreport-cli"
-	end
+    url "https://github.com/tokisatomiztsuru/interimreport-cli/archive/0.0.2.tar.gz"
+    sha256 "11cb83e3649a9f5dbe13bcbe541febbf5b90b1dd9f52644574cb691c593ddbc4"
+    version "0.0.2"
+    def install
+        bin.install "./bin/interimreport-cli"
+    end
 end
